50a416dbfc7c7bea42d79e83aad84dda16be50a8
Michael Schubert <mschubert@pivotal.io>
2015-07-10 21:42:36 +0000

    Fix broken link to epel-release for development environment

--- bin/vagrant_setup_centos6.5.sh
+++ bin/vagrant_setup_centos6.5.sh
@@ -16,7 +16,7 @@ echo "Setting up Centos 6.5"
 
 if ! yum -C repolist | grep epel ; then
   rpm --import https://fedoraproject.org/static/0608B895.txt
-  rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+  rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 fi
 
 yum -y install \
